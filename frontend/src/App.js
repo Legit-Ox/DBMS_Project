aed57b7319fd8018b8f31f62f3b246a5a4583c32
Saahil <saahil.d@ahduni.edu.in>
2023-03-07 14:40:52 +0530

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,13 @@
-import React, { useState, useEffect } from "react";
+import MerchantMock from "./pages/merchantMock";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/customer/LandingPage";
 function App() {
-  const [merchants, setMerchants] = useState(false);
-  useEffect(() => {
-    getMerchant();
-  }, []);
-  function getMerchant() {
-    fetch("http://localhost:3001")
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        setMerchants(data);
-      });
-  }
-  function createMerchant() {
-    let name = prompt("Enter merchant name");
-    let email = prompt("Enter merchant email");
-    fetch("http://localhost:3001/merchants", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email }),
-    })
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        alert(data);
-        getMerchant();
-      });
-  }
-  function deleteMerchant() {
-    let id = prompt("Enter merchant id");
-    fetch(`http://localhost:3001/merchants/${id}`, {
-      method: "DELETE",
-    })
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        alert(data);
-        getMerchant();
-      });
-  }
   return (
     <div>
-      {merchants ? merchants : "There is no merchant data available"}
-      <br />
-      <button onClick={createMerchant}>Add merchant</button>
-      <br />
-      <button onClick={deleteMerchant}>Delete merchant</button>
+      <Routes>
+        <Route path="/merchant" element={<MerchantMock />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
