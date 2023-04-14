88ab4467eb4bcc45eb54ce71348735589bfacec0
Saahil <saahil.d@ahduni.edu.in>
2023-04-14 14:25:53 +0530

    Render database connected

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,10 +2,11 @@
 const { Pool } = require("pg");
 const pool = new Pool({
   user: "my_user",
-  host: "localhost",
-  database: "my_database",
-  password: "root",
+  host: "dpg-cgsg04hjvhtrd24rfsq0-a.oregon-postgres.render.com",
+  database: "my_database_6vqv",
+  password: "fDouQqIN3D5gILrmMyw8UrNcFmeRQJ0p",
   port: 5432,
+  ssl: true,
 });
 module.exports = {
   query: (text, params) => pool.query(text, params),
