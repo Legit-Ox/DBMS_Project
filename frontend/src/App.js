0cd49ca0a8f68af4e75775251ea19c3ca4553f6e
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2023-04-17 11:35:11 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import MerchantMock from "./pages/merchantMock";
 import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import LandingPage from "./pages/customer/LandingPage";
 import SingleRestaurantPage from "./pages/customer/SingleRestaurantPage";
-
+import owner_home from "./pages/restaurant/owner_home";
 import Dashboard from "./pages/restaurant/dashboard";
 import Home from "./pages/restaurant/home";
 import Login from "./pages/restaurant/login";
 import Register from "./pages/restaurant/register";
 import { useSelector } from "react-redux";
 import HomePage from "./pages/customer/HomePage";
+import OwnerHome from "./pages/restaurant/owner_home";
+import Rough from "./pages/restaurant/rough";
 const PrivateRoutes = () => {
   const { isAuth } = useSelector((state) => state.auth);
 
@@ -24,13 +25,13 @@ function App() {
   return (
     <div className="">
       <Routes>
-        <Route path="/merchant" element={<MerchantMock />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/single" element={<SingleRestaurantPage />} />
         <Route path="/home2" element={<Home />} />
         <Route path="/restaurant/:id" element={<SingleRestaurantPage />} />
-
+        <Route path="/ownerHome" element={<OwnerHome />} />
+        <Route path="/private" element={<Rough />} />
         <Route element={<PrivateRoutes />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
